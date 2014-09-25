42defea65c6f7820db43b36febea0c39e4d34681
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-09-24 23:19:28 -0400

    validations

--- js/application.js
+++ js/application.js
@@ -4,7 +4,22 @@ var Animal = Backbone.Model.extend({
     color: 'black',
     sound: 'woof'
   },
+  validate: function(attrs, options){
+    if ( !attrs.name ){
+        alert('Your animal must have a name!');
+    }
+    if ( attrs.name.length < 2 ){
+        alert('Your animal\'s name must have more than one letter!');
+    }
+  },
   sleep: function(){
     alert(this.get('name') + ' is sleeping.');
   }
 });
+
+// dog = new Animal();
+// dog.set('name','a',{validate: true});
+
+// dog.on('error', function (model, error) {
+//     console.log(error);
+// });
